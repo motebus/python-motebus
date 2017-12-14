2c85c2d1c24dcaf81bdfb2dbc4da2096e7cc5e60
motebus <32254088+motebus@users.noreply.github.com>
2017-12-14 10:49:11 +0800

    Update example.py

--- example.py
+++ example.py
@@ -1,20 +1,11 @@
 import threading,time
 import Motebus as motebus_rpc
 
+foo = {"A":"12","B":"25"}
 
-#motebus_rpc.send("AIlab@127.0.0.1","time",["now is"],10,8,15)
-#time.sleep(1)
-#motebus_rpc.send("app1@127.0.0.1","hello",["test1","test2"],10,8,15)
-#time.sleep(1)
-#motebus_rpc.send("AIlab@127.0.0.1","hello",["test1","test2"],10,8,15)
 
-#motebus_rpc.send("OpenCV@192.168.1.21","FaceDetection",['./mona.png'],10,8,15)
-#motebus_rpc.send("OpenCV@192.168.1.21","GreyScale",['./mona.png'],10,8,15)
-motebus_rpc.send("opencv@127.0.0.1","Resize",['./mona.png',100,100],10,8,15)
-#motebus_rpc.send("opencv@192.168.1.21","Resize",['./mona.png',100,100],10,8,15)
-time.sleep(1)
-while(motebus_rpc.response()== -1):
-	continue
+motebus_rpc.send("mote@127.0.0.1","add",[foo],10,8,15)
+
 print(motebus_rpc.response())
 print('poi poi poi poi')
 
