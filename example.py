45619731c0c11c70c151e7b0f6c8c182884f41c1
motebus <32254088+motebus@users.noreply.github.com>
2017-12-18 15:08:12 +0800

    Update example.py

--- example.py
+++ example.py
@@ -5,9 +5,5 @@ foo = {"A":"12","B":"25"}
 
 
 motebus_rpc.send("mote@127.0.0.1","add",[foo],10,8,15)
-
+time.sleep(5)
 print(motebus_rpc.response())
-print('poi poi poi poi')
-
-print('poi poi poi poi')
-print('poi poi poi poi')
