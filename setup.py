87b35b817a24def4f1f9d7e5a9da2665696c628b
motebus <32254088+motebus@users.noreply.github.com>
2017-12-22 14:01:32 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='Motebus',
-      version='0.2',
+      version='0.2.1',
       description='peer to peer communication protocol',
       url='https://github.com/motebus/python-motebus',
-      author='YPCloud',
+      author='LawrenceLiu',
       author_email='info@ypcloud.com',
       license='YPCloud',
       packages=['Motebus'],
